e1485a9e68c1e70233c1e23bd837567015a71888
eduardo <eduardo.ozorio@aluno.senai.br>
2025-10-07 16:20:30 -0300

    conexão com db

---
+++ back-end/conexao.py
@@ -0,0 +1,26 @@
+# pip install psycopg2 psycopg2 dotenv stremlit fastapi uvicorn requests
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+#carregar variaves do .env
+load_dotenv()
+
+params = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+}
+
+def conecta():
+    try:
+        conexao = psycopg2.connect(**params)
+        cursor = conexao.cursor()
+        print("deu certo")
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro ao conectar ao banco de dados: {erro}")
+        return None, None
+conecta()
