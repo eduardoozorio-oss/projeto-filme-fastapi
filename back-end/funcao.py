8db7026592ab653561fad8c9c4802c02c531d763
eduardo furtado <eduardo.ozorio@aluno.senai.br>
2025-10-13 13:48:36 -0300

    add função listar filmes

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -41,6 +41,22 @@ def inserir_filme(titulo, genero, ano, avaliacao):
             cursor.close()
             conexao.close()
 
-inserir_filme("avatar", "ação", 2009, 10)
+
+
+def lista_filme():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM filmes ORDER BY id"
+            )
+        
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao tentar lista filmes {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
             
+
                  
